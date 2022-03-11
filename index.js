88529f588e67831443a7ae2391ed721dfed7c88e
TheDeadScythe <knechtmaxime@gmail.com>
2022-03-11 08:38:25 +0100

    first upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const doc = new BrowserWindow({
+    width: 900,
+    height: 800,
+    resizable : true,
+    frame : true,
+    title: "NodeDoc",
+    icon: "icon.ico",
+    //devtools : false,
+  })
+
+  doc.loadFile('src/html/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
